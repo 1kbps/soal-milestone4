ded19880ff642ba70016890ed7d5ccba88f61f2d
rkwizdev <35099808+rkwizdev@users.noreply.github.com>
2018-02-07 17:32:44 +0530

    Delete config.js

--- js/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  consumer_key:         'hhWNYL0EkTOq0BDgRfLcQkau0',
-  consumer_secret:      'WJktUmoOpTtb0k3alJtkBtq6WjZM3KFPkn1b3oVCP5BZtaDFyx',
-  access_token:         '959847270379282433-llfMNamgM0EYbuRLoR3885lCgpkSs7J',
-  access_token_secret:  'sdjubMwhMnA65CvB1W7PMDH9IR9dgEnqOY8inkJiZyvEe',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-
-
-}
